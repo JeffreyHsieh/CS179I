8c5d78578b20583a959808e39361d872828aa740
johnli1101 <jli042@ucr.edu>
2017-01-30 23:22:23 -0800

    Added in the video tutorial stuff in a separte scene
    - added in folders for organization
    - added a test_world scene to try out networking stuff
    - added in video tutorial stuff to test it out

---
+++ Assets/Scripts/test_player_controller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;   //for networking functions
+
+public class test_player_controller : NetworkBehaviour {    //NetworkBehaviour for controlling local player functions
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //basically if you are not the local player then don't use this function to prevent controlling both characters
+        if(!isLocalPlayer)
+        {
+            return;
+        }
+
+        //this grabs arrow key inputs and moves by frame which is why we multiply by delta time and by the speed of how fast we want them to turn.
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        float z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+	}
+}
