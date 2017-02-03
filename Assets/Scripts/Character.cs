4fc13916fd45bd6efde7ebbd854d61106c4c3e00
Jeffrey Hsieh <jhsie007@ucr.edu>
2017-02-03 02:19:27 -0800

    Update GM, player script.

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -4,16 +4,36 @@ using UnityEngine;
 
 public class Character : MonoBehaviour {
 	public float speed;
+	public float agility;
+	public CharacterState player;
 	private Rigidbody body;
 
-	void Start () {
-		body = GetComponent<Rigidbody> ();
+	void Start() {
+		body = GetComponent<Rigidbody>();
 	}
+	void Update () {
+		// Fetch player state.
+		player = GameObject.Find ("GameManager").GetComponent<GameManager> ().player;
+		switch (player) {
 
-	void FixedUpdate () {
-		float moveX = Input.GetAxis ("Horizontal");
-		float moveY = Input.GetAxis ("Vertical");
-		Vector3 movement = new Vector3 (moveX, 0.0f, moveY);
-		body.AddForce (movement * speed);
+		// Allow movement in move state.
+		case CharacterState.Move:
+			float movementX = Input.GetAxis ("Horizontal");
+			float movementY = Input.GetAxis ("Vertical");
+
+			transform.Rotate (0, movementX * Time.deltaTime * speed, 0);
+			transform.Translate (0, 0, movementY * Time.deltaTime * speed);
+
+			// Jump
+			if (Input.GetButtonDown("Jump")) {
+				Debug.Log("Jump");
+				body.AddForce (new Vector3(0, agility, 0), ForceMode.Impulse);
+			}
+			break;
+		case CharacterState.Interact:
+			break;
+		default:
+			break;
+		}
 	}
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,14 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum CharacterState{Move, Interact};
+
 public class GameManager : MonoBehaviour {
 
+	public CharacterState player;
 
 	void Start () {
-		
+		// Set initial state to movement.
+		player = CharacterState.Move;
 	}
 
 	void Update () {
-		
+		switch(player){
+		case CharacterState.Move:
+			// If the player initiates interaction UI, switch to interact state.
+			if(Input.GetButtonDown("Space")){
+				player = CharacterState.Interact;
+			}
+			break;
+		case CharacterState.Interact:
+			// If the player terminates interaction UI, switch to move state.
+			if(Input.GetButtonDown("Space")){
+				player = CharacterState.Move;
+			}
+			break;
+		default:
+			player = CharacterState.Move;
+			break;
+		}
 	}
 }
