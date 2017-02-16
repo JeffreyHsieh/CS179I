b70a01bafacdd4c00aca3c8d02f5c9209f93c823
Jeffrey Hsieh <jhsie007@ucr.edu>
2017-02-15 18:20:31 -0800

    Created Menu Scene

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LoadScene : MonoBehaviour {
+    public void LoadByIndex(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+}
