733da8980f042f8b207429732f5d2f2717a62740
Scientific-Inquiry <kmena003@ucr.edu>
2017-03-01 16:50:32 -0800

    sending audio and images

--- Assets/Scripts/ServerClient/client.php
+++ Assets/Scripts/ServerClient/client.php
@@ -1,26 +1,42 @@
 <?php
-    $host = "10.25.28.210";
+    $host = "10.25.2.107";
     $port = 7777;
 
     $client_socket = socket_create(AF_INET, SOCK_STREAM,0) or die("Could not create socket\n");
-    socket_connect ($client_socket , $host, $port ) ;
+    socket_connect ($client_socket , $host, $port);
 
-    $in = "photo";
+    $in = "video";
 
     socket_write($client_socket, $in, strlen($in));
 
     $buffer = null;
+    $ext = null;
+    socket_recv($client_socket, $ext, 8, 0);
+    echo $ext;
+    socket_recv($client_socket, $buffer, 13421776, 0);
 
-    socket_recv($client_socket, $buffer, 80000, 0);
+    echo "after recv";
+    $myfile = fopen("test.txt", "w");
+    fwrite($myfile,$buffer);
+    $imageData = null
+    switch ($ext){
+        case ".png":
+        case ".jpeg":
+        case ".gif":
+        case ".bmp":
+            $imgData = base64_encode($buffer);
+            $img = "<img src= 'data:image/".$ext.";base64, $imgData'>";
+            break;
+        default:
+            $imageData = $buffer;
+            $img = "<img src= 'data:image/".$ext.";base64, $imgData'>";
+            break;
+    }
+    
 
-    //echo $buffer;
-    $imgData = base64_encode($buffer);
-    $img = "<img src= 'data:image/png;base64, $imgData' />";
+   
     print($img);
-
-
-    $fp = fopen('data.png', 'w');
+    $fp = fopen("data".$ext, 'w');
     fwrite($fp, $buffer);
     fclose($fp);
-
 ?>
