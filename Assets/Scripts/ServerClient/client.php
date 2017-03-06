84cbf6450993b40427b8d30195c751c26113e9c0
Scientific-Inquiry <kmena003@ucr.edu>
2017-03-06 15:21:56 -0800

    added option for unity

--- Assets/Scripts/ServerClient/client.php
+++ Assets/Scripts/ServerClient/client.php
@@ -1,18 +1,18 @@
 <?php
-    $host = "10.25.64.238";
+    $host = "10.25.1.114";
     $port = 7777;
-
+    $option = $_REQUEST["option_post"];
+    $in = "photo";
     $client_socket = socket_create(AF_INET, SOCK_STREAM,0) or die("Could not create socket\n");
     socket_connect ($client_socket , $host, $port);
 
-    $in = "music";
 
-    socket_write($client_socket, $in, strlen($in));
+    socket_write($client_socket, $option, strlen($in));
 
     $buffer = null;
     $ext = null;
     socket_recv($client_socket, $ext, 8, 0);
-    echo $ext;
+    //echo $ext;
     socket_recv($client_socket, $buffer, 13421776, 0);
 
     #$myfile = fopen("test.txt", "w");
@@ -25,15 +25,34 @@
         case ".bmp":
             $imgData = base64_encode($buffer);
             $img = "<img src= 'data:image/".$ext.";base64, $imgData'>";
+            
+            print($img);
+            print($buffer);
+            break;
+        case ".ogg":
+            #$zip = new ZipArchive;
+            #$result = $zip->open("file.zip");
+            #if($result == TRUE){
+            #    $zip->extractTo('/');
+            #    $zip->close();
+            #}
+            $filename = "woot.ogg";
+            $psp = "data.flv";
+            header("Content-type: video/ogg");
+            header("Content-Disposition:attachment;filename=$filename");
+            header("Content-length: " . filesize($psp) . "\n\n"); 
+            echo file_get_contents($psp);
+
             break;
         default:
-            $imageData = $buffer;
+            $imgData = $buffer;
             $img = "<img src= 'data:image/".$ext.";base64, $imgData'>";
+            print($img);
     }
     
 
    
-    print($img);
+    
     $fp = fopen("data".$ext, 'w');
     fwrite($fp, $buffer);
     fclose($fp);
