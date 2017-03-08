5571637c64f3f4527c2fd1de8359731aa8556131
Scientific-Inquiry <kmena003@ucr.edu>
2017-03-08 12:13:53 -0800

    loads videos locally w/ path only

--- Assets/Scripts/Functionality/video_player.cs
+++ Assets/Scripts/Functionality/video_player.cs
@@ -2,23 +2,75 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+//using UnityEngine.GUITexture;
 
 [RequireComponent (typeof(AudioSource))]
 
 public class video_player : MonoBehaviour {
     public MovieTexture video;
     private AudioSource audio;
-
     
+    public string url;
+    //public string path = ""
+    //public string url = @"file:///C:\Users\domop_000\Videos\Short_funny_video.ogv";
+
 	// Use this for initialization
 	void Start () {
+        StartCoroutine(WWW_load_video());
         //GetComponent<Renderer>().material= video as MovieTexture;
+        /*
         video = ((MovieTexture)GetComponent<Renderer>().material.mainTexture);//.Play();
         audio = GetComponent<AudioSource>();
         audio.clip = video.audioClip;
-        audio.Play();
+        audio.Play();*/
 	}
+
+    void Unity_load_video(){
+
+        GetComponent<MeshRenderer>().material.mainTexture = video;
+
+        audio = GetComponent<AudioSource>();
+        audio.clip = video.audioClip;
+        audio.Play ();
+        video.Play ();
+     
+    }
 	
+    IEnumerator WWW_load_video(){
+
+        //if(targetRender ==null) targetRender = GetComponent<MeshRenderer> ();
+       // if(audio ==null) 
+        //url = "http://www.unity3d.com/Movie/sample.ogg";
+        url = @url;
+        Debug.Log("URL = "+url);
+        WWW www = new WWW (url);
+        yield return www;
+        
+        Debug.Log("Video downloaded");
+        if(www.movie == null)
+            Debug.Log("It was a lie");
+        video = www.movie;
+        
+        if(video.isReadyToPlay)
+            Debug.Log("Video is ready");
+        while (!video.isReadyToPlay) {
+            yield return null;
+            Debug.Log("Loading");
+        }
+
+        GetComponent<MeshRenderer>().material.mainTexture = video;
+        audio = GetComponent<AudioSource>();
+
+        if(video.audioClip == null){
+            Debug.Log("null");
+        }
+        
+        audio.clip = video.audioClip;
+        audio.Play ();
+        video.Play ();
+        yield return null;
+    }
+
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKeyDown(KeyCode.Tab) && video.isPlaying)
@@ -35,5 +87,10 @@ public class video_player : MonoBehaviour {
             audio.Play();
             video.Play();
         }
+
+        else if(Input.GetKeyDown(KeyCode.Z)){
+            StartCoroutine(WWW_load_video());
+        }
     }
+
 }
